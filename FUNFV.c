2328fc6eb67cb2f302413827fa61bddb6a4f7965
Leonardo Linguaglossa <theleos88@gmail.com>
2019-02-02 17:18:19 +0100

    Segfault to find

--- FUNFV.c
+++ FUNFV.c
@@ -31,15 +31,75 @@
 /* Global variables*/ 
 thread_t		vnf[2];		//TODO: Add parameter here.
 struct rte_ring *ring;		//TODO: add parametrized array here.
+static volatile int counter=0;	//TODO: Get rid of globals
 
+/* TODO: Temporary structures*/
+typedef struct fake_d{
+	int ipsrc;
+	int ipdst;
+	uint16_t portsrc;
+	uint16_t portdst;
+	int protocol;
+	int payload;
+} packet_data_t;
 
 
 static void handler(int sig){
 	unsigned lcore_id = rte_lcore_id();
-	printf("Received SigINT %d: exiting At core %d\n", sig, lcore_id);
+	printf("Received SigINT %d: exiting At core %d With %d packets\n", sig, lcore_id, counter);
 	exit(0);
 }
 
+
+static int transmitter (void *args){
+	thread_t *info = (thread_t*)args;
+	printf("Thread %d of type %c\n", info->id, info->type);
+
+	int i=0, count=0;
+	for (;;){
+		packet_data_t data[128];
+		for (i=0;i<128;i++){
+			data[i].payload=i;
+		}
+		count+=128;
+
+		rte_ring_enqueue_burst(info->tx_ring, (void *)data, 128, 0);
+
+	}
+
+	return 0;
+}
+
+
+static int receiver (void* args){
+	thread_t *info = (thread_t*)args;
+	printf("Thread %d of type %c\n", info->id, info->type);
+	int nb_rx;
+
+	for (;;){
+		packet_data_t data[BURST_SIZE];
+		nb_rx = rte_ring_dequeue_burst(info->rx_ring, (void *)data, BURST_SIZE, NULL);
+		counter+=nb_rx;
+	}
+	return 0;
+}
+
+static int launcher (void* args){
+	thread_t *info = (thread_t*) args;
+	int i=0;
+	for (i=0;i<2;i++){	//TODO: create
+		if (i==0){
+			transmitter((void*)&info[0]);
+		}
+		if (i==1){
+			receiver((void*)&info[1]);
+		}
+	}
+	return 0;
+}
+
+
+#ifdef IGNORE
 static int
 lcore_hello(__attribute__((unused)) void *arg)
 {
@@ -51,7 +111,6 @@ lcore_hello(__attribute__((unused)) void *arg)
 	return 0;
 }
 
-#ifdef IGNORE
 
 static void nfv_block_function( __attribute__((unused)) void *dummy)
 {
@@ -64,7 +123,6 @@ static void nfv_block_function( __attribute__((unused)) void *dummy)
 
 	/*Main loop forever*/
 	for (;;) {
-		nb_rx = rte_ring_dequeue_burst(conf->ring, (void *)bufs, burst_size);
 		if (unlikely(nb_rx == 0))
 			continue;
 	}
@@ -98,9 +156,11 @@ main(int argc, char **argv)
 	vnf[1].rx_ring=ring;
 
 
-	rte_eal_mp_remote_launch(lcore_hello, NULL, CALL_MASTER);
+//	rte_eal_mp_remote_launch(transmitter, (void*)&vnf[0], CALL_MASTER);
+	rte_eal_mp_remote_launch(launcher,  (void*)vnf, SKIP_MASTER);
+//	rte_eal_remote_launch(transmitter, (void*)&vnf[0], 9);
+//	rte_eal_remote_launch(receiver, ( (void*)&vnf[1]), 10);
 	printf("This is main at core %d\n", lcore_id);
-//	nfv_block_function();
 
 	rte_eal_mp_wait_lcore();
 	return 0;
